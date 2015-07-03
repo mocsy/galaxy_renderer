9421dee194f1764abf00711dff07375cff9dedfe
Dirk Gustke <code@asm68k.org>
2015-07-03 21:32:23 +0200

    master - added galaxy to app

--- include/app.h
+++ include/app.h
@@ -9,9 +9,12 @@
 #define INCLUDE_APP_H_
 
 
+#include <cinttypes>
+
+
 #include "core/window.h"
 #include "core/glfw.h"
-#include <cinttypes>
+#include "Galaxy.h"
 
 
 class app {
@@ -31,6 +34,7 @@ class app {
   std::uint64_t m_number_of_stars;
   core::glfw m_glfw;
   core::window m_window;
+  Galaxy m_galaxy;
 };
 
 
--- src/app.cpp
+++ src/app.cpp
@@ -19,6 +19,18 @@ app::app(std::uint64_t const number_of_stars)
 
 
 void app::init() {
+  m_glfw.init();
+  m_window.init();
+
+  m_galaxy.Reset(13000,    // radius of the galaxy
+    4000,     // radius of the core
+    0.0004,   // angluar offset of the density wave per parsec of radius
+    0.9,      // excentricity at the edge of the core
+    0.9,      // excentricity at the edge of the disk
+    0.5,
+    200,      // orbital velocity at the edge of the core
+    300,      // orbital velovity at the edge of the disk
+    30000);   // total number of stars
 
 }
 

cd6232462b39955489e2de5972298da7ebb918a8
Dirk Gustke <code@asm68k.org>
2015-07-03 14:35:33 +0200

    master - changed format, used namespace instead of class, omitted glm constants, osed fixed value for seconds per year

--- include/Constants.h
+++ include/Constants.h
@@ -1,26 +1,24 @@
 #ifndef _CONSTANTS_H
 #define _CONSTANTS_H
 
-#include <cmath>
 
+/**
+ * relevant (known) constants
+ */
+namespace constants {
+  // Convert kilometers per parsec
+  const double PC_TO_KM  = 3.08567758129e13;
 
-//--------------------------------------------------------------------------
-/** \brief A class to store relevant constants. */
-class Constant
-{
-public:
+  // Seconds per year
+  const double SEC_PER_YEAR = 31556926;
 
-  /** \brief Convert parsec to kilometre. */
-  static const double PC_TO_KM  = 3.08567758129e13;
+  // Deg to radian conversion faktor
+  // use glm
 
-  /** \brief Seconds per year. */
-  static const double SEC_PER_YEAR = 365.25 * 86400;
+  // Radian to deg conversion faktor
+  // use glm
+}
 
-  /** \brief Deg to radian conversion faktor. */
-  static const double DEG_TO_RAD = M_PI / 180.0;
-
-  /** \brief Radian to deg conversion faktor. */
-  static const double RAD_TO_DEG = 180.0 / M_PI;
-};
 
 #endif // _CONSTANTS_H
+
